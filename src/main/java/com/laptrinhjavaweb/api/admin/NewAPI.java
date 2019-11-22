a816a0b72e3c49aa5249fa17ef3e2b51b31143b4
HoangPhuc <Hoangphuc735@gmail.com>
2019-11-22 17:54:39 +0700

    create branch restful_web_service

---
+++ src/main/java/com/laptrinhjavaweb/api/admin/NewAPI.java
@@ -0,0 +1,28 @@
+package com.laptrinhjavaweb.api.admin;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.laptrinhjavaweb.dto.NewDTO;
+
+@RestController(value = "NewAPIOfAdmin")
+public class NewAPI {
+	
+	@PostMapping("/api/new")
+	public NewDTO createNew(@RequestBody NewDTO newDTO) {
+		return newDTO;
+	}
+	
+	@PutMapping
+	public NewDTO updateNew(@RequestBody NewDTO newDTO) {
+		return newDTO;
+	}
+	
+	@DeleteMapping
+	public void deleteNew(@RequestBody long[] ids) {
+		
+	}
+}
